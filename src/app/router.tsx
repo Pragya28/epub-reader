71634681048d60275fbdc53af29fe47f244d1ee1
Pragya28 <pragyajha2812@gmail.com>
2026-05-31 13:27:55 +0530

    updated default routing to library

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,5 +1,5 @@
 import type { FC } from "react";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import { LibraryScreen } from "./screens/library-screen";
 import { ReaderScreen } from "./screens/reader-screen";
 import { SettingsScreen } from "./screens/settings-screen";
@@ -8,9 +8,11 @@ import { ROUTES } from "@/shared/utils/routes";
 export const Router: FC = () => {
   return (
     <Routes>
+      <Route path="/" element={<Navigate to={ROUTES.LIBRARY} replace />} />
       <Route path={ROUTES.LIBRARY} element={<LibraryScreen />} />
       <Route path={ROUTES.READER} element={<ReaderScreen />} />
       <Route path={ROUTES.SETTINGS} element={<SettingsScreen />} />
+      <Route path="*" element={<Navigate to={ROUTES.LIBRARY} replace />} />
     </Routes>
   );
 };
